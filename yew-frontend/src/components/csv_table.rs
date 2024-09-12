a2ef2807b786d8a87291d2beadd08768b536b189
Christian M <chriamue@gmail.com>
2024-09-12 20:48:52 +0200

    add bootstrap for table

--- yew-frontend/src/components/csv_table.rs
+++ yew-frontend/src/components/csv_table.rs
@@ -12,18 +12,18 @@ pub fn csv_table(props: &CsvTableProps) -> Html {
     let tbody = props.rows.iter().map(|row| {
         html! {
             <tr>
-                <td>{row.catchdate.to_string()}</td>
-                <td><a href={row.url_raw.to_string()}>{&row.url_raw}</a></td>
+                <td>{ row.catchdate.to_string() }</td>
+                <td><a href={ row.url_raw.to_string() } target="_blank">{ &row.url_raw }</a></td>
             </tr>
         }
     });
 
     html! {
-        <div class="csv-table">
-            <table>
-                <thead>
+        <div class="table-responsive">
+            <table class="table table-striped table-hover">
+                <thead class="thead-dark">
                     <tr>
-                        { for props.header.iter().map(|header| html! { <th>{header}</th> }) }
+                        { for props.header.iter().map(|header| html! { <th>{ header }</th> }) }
                     </tr>
                 </thead>
                 <tbody>
